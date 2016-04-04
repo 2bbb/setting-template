6c9feb072516852d677fab658e1c750ff0f62395
ISHII 2bit <ishii@buffer-renaiss.com>
2016-04-04 19:18:00 +0900

    add future: template from object

--- example/index.js
+++ example/index.js
@@ -1,1 +1,6 @@
-require('../index.js')(__dirname + '/setting_template.js', __dirname + '/setting.js');
+var setting_template = require('../index.js');
+setting_template(__dirname + '/setting_template.js', __dirname + '/setting.js');
+setting_template({
+    foo: 'foo',
+    bar: 1
+}, __dirname + '/setting_from_json.json');
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 (function() {
     "use strict";
     let fs = require('fs');
-    function SettingTemplate(template_path, setting_path) {
+    function SettingTemplate(template_path_or_object, setting_path) {
         "use strict";
-        if(!fs.existsSync(setting_path)) {
-            fs.writeFileSync(setting_path, fs.readFileSync(template_path));
+        if (!fs.existsSync(setting_path)) {
+            if(typeof template_path_or_object == 'string') {
+                fs.writeFileSync(setting_path, fs.readFileSync(template_path));
+            } else {
+                fs.writeFileSync(setting_path, JSON.stringify(template_path_or_object, null, "\t"));
+            }
         }
     }
     module.exports = SettingTemplate;
